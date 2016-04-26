e2140492942fe352aeccc458946eaef7913548d5
Mario <mario.aguilar@planetmedia.com.mx>
2016-04-26 14:53:29 -0500

    song fisrt Commit

--- app/src/main/java/com/example/marioaguilar/androidsongplayer/Song_Activity.java
+++ app/src/main/java/com/example/marioaguilar/androidsongplayer/Song_Activity.java
@@ -11,6 +11,7 @@ public class Song_Activity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_song_);
+        
     }
 
     @Override
